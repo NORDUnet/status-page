8556ed0f589eba5b5f15afdbda7d60660549147a
Markus Krogh <markus@nordu.net>
2020-03-12 09:59:09 +0100

    Add zoom sub page and arguments

--- status.py
+++ status.py
@@ -1,27 +1,52 @@
 import os
+import argparse
 import yaml
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('templates'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('index.html')
+def gen_page(env, page, data, out_dir):
+    path = os.path.join(out_dir, page)
+    with open(path, 'w') as f:
+        template = env.get_template(page)
+        f.write(template.render(**data))
+        print('Created,', path)
 
-if not os.path.exists('static'):
-    os.mkdir('static')
 
-with open('data.yml', 'r') as f:
-    data = yaml.load(f, Loader=yaml.BaseLoader)
+def main(out_dir, data_path):
+    env = Environment(
+        loader=FileSystemLoader('templates'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-service_status = {}
-for event in reversed(data.get('current', [])):
-    for product in event.get('products', []):
-        service_status[product] = event.get('what', 'operational')
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
 
-data['service_status'] = service_status
+    with open(data_path, 'r') as f:
+        data = yaml.load(f, Loader=yaml.BaseLoader)
 
-with open('static/index.html', 'w') as f:
-    f.write(template.render(**data))
-print('Created static/index.html')
+    service_status = {}
+    for event in reversed(data.get('current', [])):
+        for product in event.get('products', []):
+            service_status[product] = event.get('what', 'operational')
+
+    data['service_status'] = service_status
+
+    gen_page(env, 'index.html', data, out_dir)
+    gen_page(env, 'zoom.html', data, out_dir)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-d',
+        '--data',
+        default='data.yml',
+        help='Which data file to use when gennerating pages')
+    parser.add_argument(
+        '-o',
+        '--out',
+        default='static',
+        help='Path to output directory.')
+
+    args = parser.parse_args()
+    main(args.out, args.data)
