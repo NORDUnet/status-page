595288fc05c7090750bc3b4951dfd12450ef574b
Markus Krogh <markus@nordu.net>
2020-03-13 12:29:01 +0100

    Monitor templates dir as well

--- dev.sh
+++ dev.sh
@@ -3,4 +3,4 @@
 FILE=${1:-data.yml}
 echo "Watching ${FILE} for changes"
 python status.py --data "$FILE" --dev
-fswatch -o --event PlatformSpecific "$FILE" | xargs -I{} -- python status.py --data "$FILE" --dev
+fswatch -o --event PlatformSpecific -r "templates/" "$FILE" | xargs -I{} -- python status.py --data "$FILE" --dev
